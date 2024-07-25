5df5de2b6a19c4efb807a16a148b894d96211fc9
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2024-07-25 06:24:40 -0400

    full jwt authentication and authorization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { AuthResponseDto } from './auth.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   async signIn(
     @Body('username') username: string,
